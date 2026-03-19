48e365e2bdecaef26686a010fb80f3d10b3c6ec8
moraa <mercymoraa012@gmail.com>
2026-03-19 21:00:59 +0300

    Add vercel configuration and requirements

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from app import create_app
+import os
 
 # Create application instance
-app = create_app('development')
+app = create_app(os.getenv('FLASK_ENV', 'production'))
+
+# This is critical for Vercel
+app = app
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
